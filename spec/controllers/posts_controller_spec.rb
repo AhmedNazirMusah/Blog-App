72a34ed5879dc026620c94940d149861c6c13cc7
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-01-24 21:20:07 +0000

    Fix linter errors

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,30 +1,35 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "PostsController", :type => :request do
-   describe 'post controller' do
-    it "redirects to the Post show page " do
-        get "/users/:user_id/posts/:id"
-        expect(response).to render_template(:show)
+RSpec.describe 'PostsController', type: :request do
+  describe 'post controller' do
+    it 'redirects to the Post show page ' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to render_template(:show)
     end
 
     it "redirects to the Post 'index' page" do
-        get "/users/:user_id/posts"
-        expect(response).to render_template(:index)
+      get '/users/:user_id/posts'
+      expect(response).to render_template(:index)
     end
 
     it 'confirms status code ok for post show page' do
-        get "/users/:user_id/posts/:id"
-        expect(response).to have_http_status(:ok)
-      end
-
-    it "show the exact text on post index page" do
-        get "/users/:user_id/posts/:id"
-        expect(response.body).to include("Show method for posts")
+      get '/users/:user_id/posts/:id'
+      expect(response).to have_http_status(:ok)
     end
 
-    it "does not render a different template" do
-        get "/users/:user_id/posts/:id"
-        expect(response).to_not render_template(:index)
+    it 'show the exact text on post show page' do
+      get '/users/:user_id/posts/:id'
+      expect(response.body).to include('Show method for posts')
+    end
+
+    it 'show the exact text on post index page' do
+      get '/users/:user_id/posts'
+      expect(response.body).to include('Here is a list of posts for a given user')
+    end
+
+    it 'does not render a different template' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to_not render_template(:index)
     end
   end
 end
