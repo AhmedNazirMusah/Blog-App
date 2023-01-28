5d7d810dc130a815f69e7d89555a5edeca611e7b
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-01-28 11:47:33 +0000

    Add likes controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,15 @@
+class LikesController < ApplicationController
+    def new
+      @like = Like.new
+    end
+  
+    def create
+      @like = Like.new(post_id: params[:id], author: current_user)
+      if @like.save
+        redirect_to root_path, notice: 'Liked a post'
+      else
+        render :new, alert: 'Failed to like a post. Try again'
+      end
+    end
+  end
+  
