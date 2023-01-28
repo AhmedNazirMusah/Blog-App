52b2f341a413c556a2c2d8ca6d4d89f0e0515a2c
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-01-28 12:03:13 +0000

    Update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,25 @@ class PostsController < ApplicationController
     @single_post = Post.find(params[:id].to_i)
     @post_comments = Comment.where(post: @single_post)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    if @post.save
+      redirect_to user_path(current_user.id), notice: 'Post added'
+    else
+
+      render :new, alert: 'An Error occured. Try again'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:author, :title, :text)
+  end
 end
