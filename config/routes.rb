5a13554025ea99a71e2450d98737df696558f6dd
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-01-28 18:46:11 +0000

    Fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,22 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  
   root "users#index"
-  get "users/:id" => "users#show"
-  get "users/:user_id/posts" => "posts#index", as: :users_posts
-  get "users/:user_id/posts/:id" => "posts#show", as: :users_posts_id
- 
+  
+  put 'user/:user_id/post/:id/like', to: 'posts#like', as: 'like' 
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
+  end
 end
+  
+#   root "users#index"
+#   get "users/:id" => "users#show"
+#   get "users/:user_id/posts" => "posts#index", as: :users_posts
+#   get "users/:user_id/posts/:id" => "posts#show", as: :users_posts_id
+ 
+# end
